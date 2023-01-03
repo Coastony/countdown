28076c66c97c8e59e9ccb0fbfb644ed02e6de62f
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2023-01-02 23:18:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Jan 1, 2024 00:00:00").getTime();
+    const countDate = new Date("2024-01-01").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
